ddff2b62c1a29404b09329bdce4673c7c9a58372
WangHao <54601199+frank-wh@users.noreply.github.com>
2021-05-12 11:07:08 -0400

    Create standings.js

---
+++ server/routes/standings.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const redis = require('async-redis');
+const client = redis.createClient();
+
+router.get('/:id', async (req, res) => {
+    try {
+        const cacheForStandings = await client.get(`standings${req.params.id}`);
+        if (cacheForStandings) {
+            return res.send(cacheForStandings);
+        }
+
+        const apiKey = '';
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                "x-rapidapi-host": "v3.football.api-sports.io",
+                "x-rapidapi-key": apiKey
+            }
+        };
+
+        // const leagueIdList = [61,62,71,78,79,80,88,94,98,103,106,113,119,128,135,140,141,
+        // 144,169,179,188,203,207,218,253,262,283,292,357];
+        
+        // saudi arabia - season 2019  - 307
+        let url;
+        if (req.params.id === '307') {
+            url = `https://v3.football.api-sports.io/standings?league=307&season=2019`;
+        } else {
+            url = `https://v3.football.api-sports.io/standings?league=${req.params.id}&season=2020`;
+        }
+        const { data: { response: [data] } } = await axios.get(url, requestOptions);
+        // await client.set(`standings${req.params.id}`, JSON.stringify(data), 'EX', 60 * 60 * 24);
+        await client.set(`standings${req.params.id}`, JSON.stringify(data));
+        res.json(data);
+    } catch (e) {
+	    res.sendStatus(500);
+    }
+});
+
+module.exports = router;
