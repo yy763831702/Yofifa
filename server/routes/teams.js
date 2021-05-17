72af11911364d510c25acc6ffe8260f49df0a493
WangHao <54601199+frank-wh@users.noreply.github.com>
2021-05-16 22:37:23 -0400

    Add files via upload

--- server/routes/teams.js
+++ server/routes/teams.js
@@ -4,49 +4,55 @@ const data = require('../data');
 const teamData = data.teams;
 
 router.get('/:id', async (req, res) => {
-    try {
-        const id = Number(req.params.id);
-        const team = await teamData.getTeamById(id);
-        res.json(team);
-    } catch (e) {
-        res.status(404).json({ error: 'Team not found' });
-    }
+	try {
+    	const id = Number(req.params.id);
+		const team = await teamData.getTeamById(id);
+		res.json(team);
+	} catch (e) {
+		res.status(404).json({ error: 'Team not found' });
+	}
 });
 
 router.get('/name/:teamname', async (req, res) => {
-    try {
-        const team = await teamData.getTeamsByName(req.params.teamname);
-        res.json(team);
-    } catch (e) {
-        res.status(404).json({ error: 'Team not found' });
-    }
+	try {
+		const team = await teamData.getTeamsByName(req.params.teamname);
+		res.json(team);
+	} catch (e) {
+		res.status(404).json({ error: 'Team not found' });
+	}
 });
 
-router.get('/league/:id', async (req, res) => {
-    try {
-        const id = Number(req.params.id);
-        const teamList = await teamData.getTeamsByLeagueId(id);
-        res.json(teamList);
-    } catch (e) {
-        res.status(404).json({ error: 'League not found' });
-    }
+router.get('/:league/:teamname', async (req, res) => {
+	try {
+    	const leagueId = Number(req.params.league);
+		const teamname = decodeURIComponent(req.params.teamname);
+		let team;
+		team = await teamData.getTeamsByName(teamname);
+		if (!team) {
+			const name = await teamData.getTeamByFuzzySearch(leagueId, teamname);
+			team = await teamData.getTeamsByName(name);
+		}
+		res.json(team);
+	} catch (e) {
+		res.status(500);
+	}
 });
 
 router.get('/', async (req, res) => {
-    try {
-        let skip = req.query.skip ? Number( req.query.skip ) : 0;
-        let take = req.query.take ? Number( req.query.take ) : 20;
-        if (!Number.isInteger(skip) || skip < 0) {
-            return res.status(400).json({ error: 'skip must be integer and must be greater than or equal to 0'});
-        }
-        if (!Number.isInteger(take) || take <= 0) {
-            return res.status(400).json({ error: 'take must be integer and must be greater than 0'});
-        }
-        const teamList = await teamData.getTeamList(skip, take);
-        res.json(teamList);
-    } catch (e) {
-        res.sendStatus(500);
-    }
+	try {
+		let skip = req.query.skip ? Number( req.query.skip ) : 0;
+		let take = req.query.take ? Number( req.query.take ) : 20;
+		if (!Number.isInteger(skip) || skip < 0) {
+			return res.status(400).json({ error: 'skip must be integer and must be greater than or equal to 0'});
+		}
+		if (!Number.isInteger(take) || take <= 0) {
+			return res.status(400).json({ error: 'take must be integer and must be greater than 0'});
+		}
+		const teamList = await teamData.getTeamList(skip, take);
+		res.json(teamList);
+	} catch (e) {
+		res.sendStatus(500);
+	}
 });
 
 module.exports = router;
