1e950066e60e9b9c207b6356e5d450c74be0035c
837220528 <44534588+837220528@users.noreply.github.com>
2021-05-19 03:18:33 +0800

    Add files via upload

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,11 +1,22 @@
 import React from 'react';
-
-import '../App.css';
+import "./Landing.css";
+import { SiFifa } from "react-icons/si";
+import { GiSoccerBall } from "react-icons/gi";
 
 function Landing() {
   return (
-    <div>
-      <h2>This is the Landing page</h2>
+    <div class="logo">
+        <div class="float-container">
+
+        <div class="float-child">
+        <GiSoccerBall className="App-logo" />
+        </div>
+  
+          <div class="float-child">
+          <SiFifa className="fifa" />
+        </div>
+  
+        </div>
     </div>
   );
 }
